a98ea4f793000e64bd13fe145adfe24658074797
Barberena Mi Municipio <elozcuro1994@gmail.com>
2026-09-10 15:16:31 -0600

    Update theme-toggle.js

--- assets/js/theme-toggle.js
+++ assets/js/theme-toggle.js
@@ -6,7 +6,7 @@
   var STORAGE_KEY = "bmm-theme";
   var root = document.documentElement;
   var LOGO_DEFAULT = "/images/logo1.webp";
-  var LOGO_NARANJA = "/images/logo.webp";
+  var LOGO_NARANJA = "/images/logo.png";
   var HERO_NARANJA = "/images/portada.png";
 
   function actualizarLogo(tema) {
